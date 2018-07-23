76f2086b3f2c313335abb4ce92eaa9f85f59ae72
Arcane1986 <richkane1986@gmail.com>
2018-07-23 12:35:17 -0400

    Master updates

---
+++ thyme-example/src/main/java/com/rich/thymeexample/ThymeExampleApplication.java
@@ -0,0 +1,12 @@
+package com.rich.thymeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeExampleApplication.class, args);
+	}
+}
---
+++ thyme-example/src/test/java/com/rich/thymeexample/ThymeExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rich.thymeexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
